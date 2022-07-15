8838b7f4ac0c221b42b0b5bfeb087a118ab8c515
Ivan Simeonov <ivan.simeonov@frox.ch>
2022-07-15 16:48:49 +0300

    Kubernetes profile added; minor fixes

--- pls-calculate-parking-fee/src/main/java/com/ivssim/calculateparkingfee/CalculateParkingFeeApplication.java
+++ pls-calculate-parking-fee/src/main/java/com/ivssim/calculateparkingfee/CalculateParkingFeeApplication.java
@@ -3,6 +3,8 @@ package com.ivssim.calculateparkingfee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
@@ -10,6 +12,9 @@ import reactivefeign.spring.config.EnableReactiveFeignClients;
 @EnableReactiveFeignClients(
         basePackages = "com.ivssim.clients"
 )
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CalculateParkingFeeApplication {
         public static void main(String[] args) {
             SpringApplication.run(CalculateParkingFeeApplication.class, args);
--- pls-clients/src/main/java/com/ivssim/clients/vehicle/VehicleClient.java
+++ pls-clients/src/main/java/com/ivssim/clients/vehicle/VehicleClient.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import reactivefeign.spring.config.ReactiveFeignClient;
 import reactor.core.publisher.Mono;
 
-@ReactiveFeignClient(name = "vehicle", url = "http://pls-vehicle-service")
+//@ReactiveFeignClient(name = "vehicle", url = "http://pls-vehicle-service:8080")
+@ReactiveFeignClient(name = "vehicle", url = "${clients.vehicle.url}")
 public interface VehicleClient {
 
     @GetMapping("/vehicles/{id}")
